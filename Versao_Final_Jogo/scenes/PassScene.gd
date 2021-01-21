7cc8b8cd95207c082687d53c69163d37963560d1
KrugerDaniel <62075522+KrugerDaniel@users.noreply.github.com>
2021-01-21 16:55:07 -0300

    Delete Versao_Final_Jogo directory

--- Versao_Final_Jogo/scenes/PassScene.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-var change_scene
-
-func _physics_process(_delta):
-	var bodies = get_overlapping_bodies()
-	for body in bodies:
-		if body.name == "player":
-			change_scene = get_tree().change_scene("res://scenes/Fase_2.tscn")
